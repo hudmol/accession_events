7d76460f527538ba96694d1ddb6d610821704803
jambun <jambun@gmail.com>
2015-05-12 12:52:17 -0400

    add support for agent_uri in config instead of agent_username improve config error reporting

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -2,10 +2,11 @@
 
 # This is a full set of default config values based on NLA's requirements
 config_defaults = {
-  :agent => 'admin',
+  :agent_username => 'admin', # specify agent_username if the agent is a user
+#  :agent_uri => '/agents/people/1', # otherwise specify the agent's backend uri
   :agent_role => 'authorizer',
   :accession_role => 'source',
-  :outcome => '',
+  :outcome => 'fail',
   :event_types => [
                    'acknowledgement_sent',
                    'agreement_sent',
@@ -22,6 +23,10 @@ AppConfig[:accession_events] = {} unless AppConfig.has_key?(:accession_events)
 AppConfig[:accession_events] = config_defaults.merge(AppConfig[:accession_events])
 
 
+class AccessionEventsConfigException < Exception
+end
+
+
 ###
 # The rest of this is checking the config
 
@@ -30,7 +35,7 @@ AppConfig[:accession_events] = config_defaults.merge(AppConfig[:accession_events
 def check_enum_value(cfg_name, enum_name, value)
   enum = Enumeration[:name => enum_name]
   if EnumerationValue.filter(:enumeration_id => enum.id).all.select { |ev| ev.value == value }.empty?
-    raise "#{cfg_name} '#{value}' provided in accession_events configuration does not exist"
+    raise AccessionEventsConfigException.new("#{cfg_name} '#{value}' does not exist")
   end
 end
 
@@ -38,12 +43,35 @@ end
 cfg = AppConfig[:accession_events]
 
 # check agent
-user = User[:username => cfg[:agent]]
-if user.nil?
-  raise "username '#{cfg[:agent]}' doesn't exist for agent in accession_events configuration"
+if cfg.has_key?(:agent_uri)
+  (leadingnil, agents, subagent, id) = cfg[:agent_uri].split('/')
+  unless agents == 'agents'
+    raise AccessionEventsConfigException.new("agent_uri #{cfg[:agent_uri]} is not a valid agent uri")
+  end
+  model = case subagent
+          when 'people'
+            AgentPerson
+          when 'corporate_entities'
+            AgentCorporateEntity
+          when 'families'
+            AgentFamily
+          when 'software'
+            AgentSoftware
+          else
+            raise AccessionEventsConfigException.new("agent_uri #{cfg[:agent_uri]} is not a valid agent uri")
+          end
+  agent = model[id.to_i]
+  if agent.nil?
+    raise AccessionEventsConfigException.new("no agent found for agent_uri #{cfg[:agent_uri]}")
+  end
+else
+  user = User[:username => cfg[:agent_username]]
+  if user.nil?
+    raise AccessionEventsConfigException.new("username '#{cfg[:agent]}' doesn't exist for agent")
+  end
+  # remember the agent_uri so we don't have to look it up again
+  cfg[:agent_uri] = JSONModel::JSONModel(:agent_person).uri_for(user.agent_record_id)
 end
-# remember the agent_uri so we don't have to look it up again
-cfg[:agent_uri] = JSONModel::JSONModel(:agent_person).uri_for(user.agent_record_id)
 
 # check agent_role
 check_enum_value('agent_role', 'linked_agent_event_roles', cfg[:agent_role])
@@ -61,9 +89,9 @@ cfg[:event_types].each do |type|
   bad_event_types << type if enum.select { |ev| ev.value == type }.empty?
 end
 if bad_event_types.length == 1
-  raise "event_type '#{bad_event_types.first}' provided in accession_events configuration does not exist"
+  raise AccessionEventsConfigException.new("event_type '#{bad_event_types.first}' does not exist")
 elsif bad_event_types.length > 1
-  raise "event_types '#{bad_event_types.join(', ')}' provided in accession_events configuration do not exist"
+  raise AccessionEventsConfigException.new("event_types '#{bad_event_types.join(', ')}' do not exist")
 end
 
 # All done
